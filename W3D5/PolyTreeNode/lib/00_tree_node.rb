84e948b0887c4e3516892c88b14944512903c655
Michael Clayton Noble <makos.code@gmail.com>
2020-12-04 14:25:28 -0800

    Implemented parent=(new_parent): passes rspec tests

--- W3D5/PolyTreeNode/lib/00_tree_node.rb
+++ W3D5/PolyTreeNode/lib/00_tree_node.rb
@@ -1,10 +1,21 @@
 class PolyTreeNode
 
-  attr_reader :parent, :children, :value
+  attr_accessor :parent, :children, :value
 
   def initialize(value)
     @value = value
     @parent = nil 
     @children = []
   end
+
+  def parent=(new_parent)
+    if new_parent
+      @parent.children.delete(self) unless !@parent
+      @parent = new_parent
+      @parent.children << self
+    else
+      @parent = nil
+    end
+  end
+
 end
