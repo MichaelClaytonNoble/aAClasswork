8dde251a96558cd5b0a6cb846d33e6ed27494139
Michael Clayton Noble <makos.code@gmail.com>
2020-12-04 15:35:47 -0800

    Implemented add_child(child), remove_child(child) methods: passes rspec tests

--- W3D5/PolyTreeNode/lib/00_tree_node.rb
+++ W3D5/PolyTreeNode/lib/00_tree_node.rb
@@ -9,13 +9,35 @@ class PolyTreeNode
   end
 
   def parent=(new_parent)
+    @parent.children.delete(self) unless !@parent
+    
     if new_parent
-      @parent.children.delete(self) unless !@parent
       @parent = new_parent
       @parent.children << self
     else
       @parent = nil
     end
+  end
+
+  def add_child(child_node)
+    child_node.parent=self
+  end
+
+  def remove_child(child_node)
+    if @children.include?(child_node)
+      
+      child_node.parent=nil
+      @children.delete(child_node)
+    else
+      raise "Not a child of mine(${self.value})"
+    end
+  end
+
+  def inspect
+    str = "parent= " 
+    @parent ? str+=@parent.value : str+='nil'
+    str+= ' \n ' + "children= " + @children.to_s
+    str
   end
 
 end
