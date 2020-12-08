758dc0f9eb3d6c86b3676ba95dd811d279e56fb5
Michael Clayton Noble <makos.code@gmail.com>
2020-12-08 14:16:56 -0800

    create ::Game

---
+++ W4D2/chess/game.rb
@@ -0,0 +1,22 @@
+class Game
+
+  def initialize#(player1, player2)
+
+    @board = ''
+    @display = '' 
+    @players = {}
+    @current_player = ''
+  end
+
+  def play
+  end
+
+  private
+  def notify_players
+  end
+
+  private
+  def swap_turn!
+  end
+
+end
