ef4d3eb4c0b56741d50d7b371a055c0186132e51
Tului2020 <Tului2020>
2020-12-08 14:00:32 -0800

    switch

--- W4D2/employee.rb
+++ W4D2/employee.rb
@@ -20,6 +20,7 @@ end
 class Manager < Employee
 
     def initialize
+        super
         @employees = []
     end
 
@@ -33,4 +34,11 @@ class Manager < Employee
 end
 
 
+ned = Employee.new("Ned", "Founder", 1000000, nil)
+darren = Manager.new("Darren", "TA Manager", 78000, ned)
+shawna = Employee.new("Shawna", "TA", 12000, darren)
+davod = Employee.new("David", "TA", 10000, darren)
 
+p ned.bonus(5) # => 500_000
+p darren.bonus(4) # => 88_000
+p david.bonus(3) # => 30_000
