3dd5d7a91cfb749c0c9c22c60393b357813f5a10
Tului2020 <Tului2020>
2020-12-08 13:55:31 -0800

    added bonus strcture for both manager and employee

--- W4D2/employee.rb
+++ W4D2/employee.rb
@@ -1,3 +1,36 @@
-test...
+class Employee
+
+    attr_reader :salary
+
+    def initialize(name, title, salary, boss)
+        @name = name
+        @title = title
+        @salary = salary
+        @boss = boss
+    end
+
+    def bonus(multiplier)
+        bonus = @salary * multiplier    
+    end
+
+
+end
+
+
+class Manager < Employee
+
+    def initialize
+        @employees = []
+    end
+
+    def bonus(multiplier)
+        total_salaries = @employees.inject(0) { |acc, e| acc + e.salary}
+        total_salaries * multiplier
+    end
+
+
+
+end
+
 
 
