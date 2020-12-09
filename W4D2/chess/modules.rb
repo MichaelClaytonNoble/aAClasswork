4454845c962d3a5048fee9524d9a34add98f7c28
Michael Clayton Noble <makos.code@gmail.com>
2020-12-09 11:16:51 -0800

    add #move_dirs, check error

--- W4D2/chess/modules.rb
+++ W4D2/chess/modules.rb
@@ -1,8 +1,8 @@
-module Slideable
+# module Slideable
   DIAGONAL_DIRS = [ [1, 1], [1, -1], [-1, -1], [-1, 1] ] 
   HORIZONTAL_DIRS = [ [1, 0], [0, 1], [-1, 0], [0, -1] ]
 
-  def horizonal_dirs
+  def horizontal_dirs
     HORIZONTAL_DIRS
   end
 
@@ -13,28 +13,35 @@ module Slideable
   def moves
   end
 
-  private
+  # private
   def move_dirs
+    horizontal_moves = []
+    diagonal_moves = []
+
+    diagonal_dirs.each {|dir| diagonal_moves << grow_unblocked_moves_in_dir(dir[0], dir[1]) }
+    horizontal_dirs.each {|dir| horizontal_moves << grow_unblocked_moves_in_dir(dir[0], dir[1]) }
+
+    [diagonal_dirs, horizontal_dirs]
   end
 
-  private
+  # private
   def grow_unblocked_moves_in_dir(dx,dy)
     potential_moves = []
     
-    new_pos = self.pos
+    new_pos = [0,0] #self.pos
     r, c = new_pos
-    while r > 0 && r < 7 && c > 0 && c < 7
+    while r >= 0 && r <= 7 && c >= 0 && c <= 7
+      potential_moves << [r, c]
       r += dx
       c += dy
-      potential_moves << [r, c]
     end
     potential_moves
-
-
   end
 
-end
+# end
 
+move_dirs.each { |e| p e}
+# p grow_unblocked_moves_in_dir(1,0)
 
 module Stepable
 
