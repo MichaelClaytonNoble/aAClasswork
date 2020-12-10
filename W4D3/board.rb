b4dcbd5c9a97ed44c53e19a16c2ad93e0ee6c363
Michael Clayton Noble <makos.code@gmail.com>
2020-12-10 02:59:12 -0800

    implement Board --> #move_piece, #setup --> can move a chess piece and populate the board with pieces

--- W4D3/board.rb
+++ W4D3/board.rb
@@ -1,27 +1,59 @@
 require_relative 'piece.rb'
+require 'singleton'
 
 class Board
 
   def initialize
-    @board = Array.new(8){Array.new(8, nil)}
+    @null_piece = NullPiece.instance
+    @board = Array.new(8){Array.new(8, @null_piece)}
+    setup
   end
 
+  #raise exception if there is no piece at start
+  #raise if piece cannot move to end pos
+  #place a begin rescue whereever this method is called. 
+  def move_piece(start_pos, end_pos)
+
+    raise "There is no piece at #{start_pos}" if self[start_pos] == @null_piece
+    raise "Piece cannot move to #{end_pos}" if out_of_bounds?(end_pos)
+    
+  
+    piece = self[start_pos]
+    self[start_pos] = @null_piece
+
+    self[end_pos] = piece
+  end
+
+  def [](pos)
+    r, c = pos
+    @board[r][c]
+  end
+
+  def []=(pos, val)
+    r, c = pos
+    @board[r][c] = val
+  end
+
+  def print_board
+    @board.each {|r| puts r.to_s}
+  end
+
+  private
+  #add pieces to the board in their appropriate start positions
   def setup
     (0..1).each do |row|
       @board[row].map!{|col| Piece.new}
-      @board[-1-i].map!{|col| Piece.new}
+      @board[-1-row].map!{|col| Piece.new}
     end
   end
 
-  def inspect
-
-    board_string = ''
-
-    @board.each {|r| board_string+=r.to_s}
-    board_string
+  def out_of_bounds?(pos)
+    r, c = pos
+    r >= 8 || r < 0 || c >= 8 || c < 0
   end
+
 end
 
 b = Board.new
 
-p b
+b.print_board
--- W4D3/piece.rb
+++ W4D3/piece.rb
@@ -1,12 +1,23 @@
+require 'singleton' 
+
 class Piece
 
+  def initialize
+  end
 
+  def moves
+  end
+
+  def inspect
+    "PIECE"
+  end
 end
 
 class NullPiece < Piece
+  include Singleton
 
-  def initialize
-    super
+  def inspect
+    "[|||]"
   end
 
 end
