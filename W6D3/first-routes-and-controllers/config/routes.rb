f1160c8b89bf62324738a1e8047c03925a1dee98
Janette <tinoconjanette@gmail.com>
2020-12-29 14:30:14 -0800

    Add routes to first_routrs

--- W6D3/first-routes-and-controllers/config/routes.rb
+++ W6D3/first-routes-and-controllers/config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # resources :users
-
-  get 'users/:id', to: 'users#show', as: 'user'
-  post 'users/', to: 'users#show', as: 'user'
-  get 'users/:id', to: 'users#show', as: 'user'
-  get 'users/:id', to: 'users#show', as: 'user'
-  get 'users/:id', to: 'users#show', as: 'user'
-  get 'users/:id', to: 'users#show', as: 'user'
-  get 'users/:id', to: 'users#show', as: 'user'
-  get 'users/:id', to: 'users#show', as: 'user'
-  get 'users/:id', to: 'users#show', as: 'user'
+  #resources :users
+  get '/users', to: 'users#index', as: 'user'
+  get '/users/new', to: 'users#new', as: 'new_user'
+  post '/users', to: 'users#create', as: 'create_user'
+  get '/users/:id', to: 'users#show', as: 'show_user'
+  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  patch '/users/:id', to: 'users#update', as: 'update_user'
+  #put '/users/:id', to: 'users#update', as: 'update_user'
+  delete '/users/:id', to: 'users#destroy', as: 'delete_user'
   
 
 
