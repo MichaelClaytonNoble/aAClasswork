3949db3f25f471dc04f8929260f109dffc6c8867
Michael Clayton Noble <makos.code@gmail.com>
2020-12-29 14:05:05 -0800

    adding routes

---
+++ W6D3/first-routes-and-controllers/config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # resources :users
+
+  get 'users/:id', to: 'users#show', as: 'user'
+  post 'users/', to: 'users#show', as: 'user'
+  get 'users/:id', to: 'users#show', as: 'user'
+  get 'users/:id', to: 'users#show', as: 'user'
+  get 'users/:id', to: 'users#show', as: 'user'
+  get 'users/:id', to: 'users#show', as: 'user'
+  get 'users/:id', to: 'users#show', as: 'user'
+  get 'users/:id', to: 'users#show', as: 'user'
+  get 'users/:id', to: 'users#show', as: 'user'
+  
+
+
+
+end
