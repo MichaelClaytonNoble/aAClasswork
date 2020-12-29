7712b24389cc1988adbad19f643e9c7e0ebcd8a0
Michael Clayton Noble <makos.code@gmail.com>
2020-12-29 14:59:32 -0800

    Add controller actions #index #create #show

--- W6D3/first-routes-and-controllers/app/controllers/users_controller.rb
+++ W6D3/first-routes-and-controllers/app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
 
 
+  def index 
+
+    # users = User.all 
+    render plain: "I MADE IT"
+  end
+
+  def create
+
+    render json: params
+  end
+
+  def show
+
+    render json: params
+  end
+
 end
