68dfa5ca2736fd8fadaa2f61ef25cb6427ad3015
Michael Clayton Noble <makos.code@gmail.com>
2020-12-29 16:49:14 -0800

    add create for user through the api

--- W6D3/first-routes-and-controllers/app/controllers/users_controller.rb
+++ W6D3/first-routes-and-controllers/app/controllers/users_controller.rb
@@ -1,20 +1,29 @@
 class UsersController < ApplicationController
 
-
   def index 
 
-    # users = User.all 
-    render plain: "I MADE IT"
+    users = User.all 
+    render json: users
   end
 
   def create
-
-    render json: params
+  
+    debugger
+    user = User.new(user_params)
+    # replace the `user_attributes_here` with the actual attribute keys
+    user.save!
+    render json: user
   end
 
   def show
 
     render json: params
   end
+
+  def user_params
+    #require that we have a top level key of :user
+    params.require(:user).permit(:name, :email)
+  end
+
 
 end
--- W6D3/first-routes-and-controllers/app/models/user.rb
+++ W6D3/first-routes-and-controllers/app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
     validates :name, presence: true
     validates :email, presence: true
 
+    def initialize(options)
+        @name = options['name']
+        @email = options['email']
+    end
+
+    
+
 end
