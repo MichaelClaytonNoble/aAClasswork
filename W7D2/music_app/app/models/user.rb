48dab4fad997a2b738aa4de92ddc47a45f9bb13e
Michael Clayton Noble <makos.code@gmail.com>
2021-01-05 11:12:37 -0800

    create user_controller and new action add new and create routes for users

--- W7D2/music_app/app/models/user.rb
+++ W7D2/music_app/app/models/user.rb
@@ -18,18 +18,43 @@ class User < ApplicationRecord
   validates :session_token, :email, uniqueness: true
   validates :password, length: {minimum: 6}, allow_nil: true
 
+  #runs ensure_session_token right after initialize
+  after_initialize :ensure_session_token
+
+  # runs ensure_session_token right before User.save 
+  # before_validation :ensure_session_token
 
   def self.generate_session_token
     SecureRandom::urlsafe_base64
   end
 
-  def reset_session_token!
-    self.session_token = User.generate_session_token
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email) 
+    if user && user.is_password(password)
+      user 
+    else
+      nil
+    end
   end
 
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
 
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
 
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+    @password = password
+  end
 
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
 
 
 
