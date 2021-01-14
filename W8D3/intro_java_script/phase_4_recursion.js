b35b9016090735634d4a152890b6ce1dae744d6f
Michael Clayton Noble <makos.code@gmail.com>
2021-01-13 17:01:24 -0800

    bsearch start merge

--- W8D3/intro_java_script/phase_4_recursion.js
+++ W8D3/intro_java_script/phase_4_recursion.js
@@ -26,11 +26,52 @@ function exponent_b(base, exp){
   if (exp === 0 ){
     return 1;
   } else if (base%2 === 0) {
+
     let root = exponent_b(base, exp/2);
     return root * root; 
-  } else {
-    let root = exponent_b(base, exp/2 )
-    return base * root * root;
-  }
+  } else { 
 
+      let root = exponent_b(base, exp/2 )
+      return base * root * root;
+  }
+}
+
+function bsearch(arr, target){
+
+  if (arr.length == 0){
+    return -1;
+  }
+  else{
+    let mid = Math.trunc(arr.length/2);
+    
+    if (arr[mid] === target){
+      return mid;
+    }
+    if (arr[mid] > target){
+        return bsearch(arr.slice(0,mid), target);
+    } 
+    else {
+      let right = bsearch(arr.slice(mid+1,arr.length), target);
+      if(right === -1){
+        return -1;
+      }
+      return mid + 1 + right; 
+    }
+  }
+}
+
+
+function mergesort(arr){
+  let mid = Math.trunc(arr.length/2)
+
+  if(arr.length === 0 ){
+    return [];
+  }
+  let left = arr.slice(0,mid);
+  let right = arr.slice(mid, arr.length);
+
+  let leftSorted = mergesort(left);
+  let rightSorted = mergesort(right);
+
+  return merge(leftSorted, rightSorted);
 }
