e2042920f1fbe99525f51b9bcbf73e8987436b25
Michael Clayton Noble <makos.code@gmail.com>
2021-01-18 15:21:25 -0800

    complete phase_1 Array#monkeypatch

--- W8D3/intro_java_script/phase_1_arrays.js
+++ W8D3/intro_java_script/phase_1_arrays.js
@@ -1,41 +1,50 @@
-Array.prototype.uniq = function(){
+//array uniq
 
-  let uniq_array = [];
+Array.prototype.uniq = function (){
+  let uniq_array = new Array;
   let uniq_hash = {};
-  for( let i = 0; i < this.length; i++){
-    if (!uniq_hash[this[i]]) {
-      uniq_hash[this[i]]=true;
-      uniq_array.push(this[i]);
-    }
-  }
+  this.forEach( (el) => { 
+    uniq_hash[el] ? '' : uniq_array.push(el);
+    uniq_hash[el] = true;
+  });
   return uniq_array;
 }
 
-Array.prototype.twoSum = function(){
+// console.log([1,2,2,3,3,3].uniq());
 
-  let pair_array = []; //pairs of indices
-  let seen_hash = {}; 
-  // [3,3,3,-3,-3,-3,-3,4]
-  // output: [[0,3],[1,3],[2,3],[0,4],[]]
-  for(let i = 0; i < this.length; i++){
-    for(let j = 0; j < this.length; j++){
-      if(i < j && this[i] + this[j] == 0){
-        pair_array.push([i,j]);
+
+Array.prototype.twoSum = function () { 
+  pairs_array = new Array;
+
+  let i = 0;
+  let j = 1;
+  for(let i=0; i<this.length-1; i++){
+    
+    for(let j=i+1; j<this.length; j++){
+
+      if(this[i] + this[j] === 0){
+        pairs_array.push( [i,j] );
       }
     }
   }
-  return pair_array;
+  return pairs_array;
 }
 
-Array.prototype.transpose = function(){
-  let transposed = [];
-  // nested loop -> column then row rather than row then column
-  for(let i = 0; i < this[0].length; i++){
-    let new_row = [];
-    for(let j = 0; j < this.length; j++){
+// console.log([1,-1,23,3,-23,4,-3,44].twoSum());
+
+Array.prototype.transpose = function (){
+
+  let transposed_matrix = new Array;
+
+  for(let i=0; i<this[0].length; i++){
+
+    let new_row = new Array;
+    for(let j=0; j<this.length; j++){
       new_row.push(this[j][i]);
     }
-    transposed.push(new_row);
+    transposed_matrix.push( new_row )
   }
-  return transposed;
+  return transposed_matrix;
 }
+
+// console.log([ [1,2,3], [4,5,6] ].transpose());
