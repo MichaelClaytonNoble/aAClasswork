0bd39e143a717995b8c479819cf4bb1d5283d0eb
Michael Clayton Noble <makos.code@gmail.com>
2021-01-21 14:40:35 -0800

    create toggle button and constructor method for the toggle

---
+++ W9D4/ajax_twitter/frontend/follow_toggle.js
@@ -0,0 +1,15 @@
+const { Module } = require("webpack");
+
+class FollowToggle{
+  constructor(){
+    this.el = $('.follow-toggle'); //finds all buttons with class name follow-toggle
+    //imbedded in this button we hid dataset 
+    //userId is stored in dataset through the button ====> data-userId = "<%= user.id %>"
+    this.userId = this.el.dataset["userId"]; //dataset seems to be a hash that stores the data
+
+    this.followState = this.el.dataset["initial-follow-state"];
+
+  }
+}
+
+module.exports = FollowToggle; //makes this class requirable 
---
+++ W9D4/ajax_twitter/frontend/twitter.js
@@ -0,0 +1,9 @@
+require("./follow_toggle")
+
+// get the 'this'.el ---> which will be a jquery object containing all follow/unfollow buttons. 
+
+()=>{
+  // loop over 'this'.el button by button 
+      //call constructor 
+
+}
