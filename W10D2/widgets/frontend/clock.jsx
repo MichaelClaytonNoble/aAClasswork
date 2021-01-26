b4bbae52d4d555fbddfd4349cc2369bd7e46cc81
Michael Clayton Noble <makos.code@gmail.com>
2021-01-26 15:24:57 -0800

    added in clock and time functionality

--- W10D2/widgets/frontend/clock.jsx
+++ W10D2/widgets/frontend/clock.jsx
@@ -6,21 +6,31 @@ class Clock extends React.Component{
   constructor(){
     super();
     this.state = {date: new Date()};
-
+    this.intervalId;
   }
 
   componentDidMount() {
-    setInterval(this.tick, 1000);
+    this.intervalId = (setInterval(this.tick.bind(this), 1000));
+  }
+  //how often is unmount being called ? 
+  componentWillUnmount(){
+    clearInterval(this.intervalId);
+    debugger;
   }
 
   tick(){
     this.setState({date: new Date()});
   }
   render(){
+
+    const seconds = this.state.date.getSeconds();
+    const minutes = this.state.date.getMinutes();
+    const hours = this.state.date.getHours();
+
     return (
     <div>
       <h1>Clock!!!!!!!!!!!!!</h1>
-      <h2>{this.state.date.map}</h2>
+      <h2>{hours} : {minutes} : {seconds}</h2>
     </div>);
   }
 
