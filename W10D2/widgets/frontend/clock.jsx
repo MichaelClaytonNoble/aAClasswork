ddfe8fc17905527d904ad79dd884746cc8a9fb92
Andrew Curd <andrewc2695@gmail.com>
2021-01-26 15:05:11 -0800

    wrote tick function for clock

--- W10D2/widgets/frontend/clock.jsx
+++ W10D2/widgets/frontend/clock.jsx
@@ -3,9 +3,24 @@ import ReactDOM from 'react-dom';
 
 class Clock extends React.Component{
 
+  constructor(){
+    super();
+    this.state = {date: new Date()};
+
+  }
+
+  componentDidMount() {
+    setInterval(this.tick, 1000);
+  }
+
+  tick(){
+    this.setState({date: new Date()});
+  }
   render(){
-    return (<div>
-      <h1>Clock</h1>
+    return (
+    <div>
+      <h1>Clock!!!!!!!!!!!!!</h1>
+      <h2>{this.state.date.map}</h2>
     </div>);
   }
 
--- W10D2/widgets/frontend/widgets.jsx
+++ W10D2/widgets/frontend/widgets.jsx
@@ -3,16 +3,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Clock from './clock';
 
+
 function Root(){
   
-  return (<div></div>);
+  return (<div>
+    <Clock />
+  </div>);
 }
 
 document.addEventListener('DOMContentLoaded', ()=> {
 
   const reactRoot = document.getElementById('react-root');
 
-  ReactDOM.render(<div>Hello</div>, reactRoot);
+  ReactDOM.render(<Root />, reactRoot);
 
 
 });
