3423e1f0fe1e316115895324434a27cd8e064695
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2021-01-27 12:21:39 -0800

    Push the rest of the files from the last commit

--- W10D3/minesweeper/components/game.jsx
+++ W10D3/minesweeper/components/game.jsx
@@ -1,23 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import * as Minesweeper from '../minesweeper.js'
+import Board from "./board";
+
 export default class Game extends React.Component{
 
   // constructor(props){
     // super(props);
     // this.state = {board: new Minesweeper.Board(this.props.gridSize, this.props.numBombs)}
   constructor(){
-    this.state = {board: new Minesweeper.Board(9, 14)}
+    this.state = {
+      board: new Minesweeper.Board(9, 14)
+    }
     this.updateGame = this.updateGame.bind(this);
   }
 
   updateGame(){ }
-
+  
   render(){
+    
     return (
-      <Board currentBoard={this.state.board} updateGame={this.updateGame}/>
-    )
-  }
-
-
+      // <Board currentBoard={this.state.board} updateGame={this.updateGame}/>
+      // <Board/>
+      <div>Hello!</div>
+      )
+    };
+    
 }
---
+++ W10D3/minesweeper/react_minesweeper.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Game from "./components/game";
+
+document.addEventListener("DOMContentLoaded", () => {
+    // debugger
+    const root = document.getElementById("react-root");
+    ReactDOM.render(<Game />, root);
+
+});
