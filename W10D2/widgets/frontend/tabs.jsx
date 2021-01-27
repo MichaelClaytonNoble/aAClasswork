d05a3cb5c2468a38b07e1e6bda423f8263e24a86
Michael Clayton Noble <makos.code@gmail.com>
2021-01-26 16:55:30 -0800

    finish clock widget

---
+++ W10D2/widgets/frontend/tabs.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Tabs extends React.Component{
+
+  constructor(){
+    super();
+    this.state = ({ selectedTab: 0});
+  }
+
+  render(){
+    return (<div>
+
+    </div>);
+  }
+
+}
+
+export default Tabs;
