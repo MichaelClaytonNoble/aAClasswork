05eee339477d68526713558344bda289500e1ba7
Michael Clayton Noble <makos.code@gmail.com>
2021-01-27 11:21:05 -0800

    create board class

---
+++ W10D3/minesweeper/components/game.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import * as Minesweeper from '../minesweeper.js'
+export default class Game extends React.Component{
+
+  // constructor(props){
+    // super(props);
+    // this.state = {board: new Minesweeper.Board(this.props.gridSize, this.props.numBombs)}
+  constructor(){
+    this.state = {board: new Minesweeper.Board(9, 14)}
+    this.updateGame = this.updateGame.bind(this);
+  }
+
+  updateGame(){ }
+
+  render(){
+    return (
+      <Board currentBoard={this.state.board} updateGame={this.updateGame}/>
+    )
+  }
+
+
+}
