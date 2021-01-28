d8b22306789ae0410eef3af37842548a66a94f1b
Michael Clayton Noble <makos.code@gmail.com>
2021-01-28 11:54:20 -0800

    setup app add reducers and store

---
+++ W10D4/todos_part1/frontend/todo_redux.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+
+  const content = document.getElementById("content");
+  ReactDOM.render(<div>HELLO</div>, content);
+
+});
+
+
