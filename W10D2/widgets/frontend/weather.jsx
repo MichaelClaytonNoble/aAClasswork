46525cfc48c3d76f2632287c0560e60e0172a79e
Michael Clayton Noble <makos.code@gmail.com>
2021-01-30 21:01:22 -0800

    create api call to open weather

--- W10D2/widgets/frontend/weather.jsx
+++ W10D2/widgets/frontend/weather.jsx
@@ -1,12 +1,51 @@
-import React from 'react'
+import React from 'react';
 
 class Weather extends React.Component{
 
   constructor(){
-
+    super();
+    this.state = {
+      weather: null
+    };
+    // this.getWeatherFromLocation();
   }
-  
+
+  componentDidMount(){
+    this.getWeatherFromLocation();
+  }
+
+
+  getWeatherFromLocation(){
+    navigator.geolocation.getCurrentPosition((geo) => {
+      const lat = geo.coords.latitude;
+      const lon = geo.coords.longitude;
+
+      var request = new XMLHttpRequest();
+      request.open('GET', `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=44f337ec0ac42522892a7ca67c58a701`, true);
+
+      request.onload = function () {
+        if (this.status >= 200 && this.status < 400) {
+          // Success!
+          var resp = this.response;
+          console.log(resp);
+        } else {
+          // We reached our target server, but it returned an error
+          console.log("could not retrieve weather information");
+        }
+      };
+
+      request.onerror = function () {
+        // There was a connection error of some sort
+        console.log("there was a connection error of some sort");
+      };
+      request.send();
+    });
+  }
+
   render(){
-    return <div>hello</div>
+    return <div>loading weather...</div>
   }
 }
+
+
+export default Weather;
--- W10D2/widgets/frontend/widgets.jsx
+++ W10D2/widgets/frontend/widgets.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Clock from './clock';
 import Tabs from './tabs'; 
+import Weather from './weather'
+
 
 const tabInfo = [{ title: "Tab 1", content: "First" }, { title: "Tab 2", content: "Second" }, { title: "Tab 3", content: "Third" }];
 
@@ -10,6 +12,7 @@ function Root(){
 
   return (<div>
     <Clock />
+    <Weather />
     <Tabs tabInfo={tabInfo} />
   </div>);
 }
