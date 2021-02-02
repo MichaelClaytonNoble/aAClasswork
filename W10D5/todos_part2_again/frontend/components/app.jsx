3651acdbf836bf2f2237dffa0f82c91fc70b316c
Michael Clayton Noble <makos.code@gmail.com>
2021-02-01 23:14:12 -0800

    begin part 2

---
+++ W10D5/todos_part2_again/frontend/components/app.jsx
@@ -0,0 +1,8 @@
+//holds all of the top-level concerns / components of the app
+//top-level concern is a feature of the app that functions on its own and is not nested under any other features. 
+
+import React from 'react';
+
+const App = ()=>{
+
+}
