7c55f6add9a885e5404a750828d8b0cd68134278
Qi Zhu <rubybfm1017@gmail.com>
2021-02-02 13:54:45 -0800

    show.json.jbuilder

--- W11D2/jbuilder/config/routes.rb
+++ W11D2/jbuilder/config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   namespace :api, defaults:{ format: :json } do
-    resources :guests, only: [:show] do
-      resources :guests, only: [:index]
+    resources :guests, only: [:index,:show] do
     end
     resources :gifts, only:[:index, :show]
     resources :parties, only: [:index, :show]
   end
   # Your routes here!
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
