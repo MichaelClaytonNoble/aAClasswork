efa68283ec3189aa60e008989230272235a5134a
Qi Zhu <rubybfm1017@gmail.com>
2021-02-02 12:19:15 -0800

    start with routes

--- W11D2/jbuilder/config/routes.rb
+++ W11D2/jbuilder/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  namespace :api, defaults:{ format: :json } do
+    resources :guests, only: [:show] do
+      resources :guests, only: [:index]
+    end
+    resources :gifts, only:[:index, :show]
+    resources :parties, only: [:index, :show]
+  end
   # Your routes here!
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
