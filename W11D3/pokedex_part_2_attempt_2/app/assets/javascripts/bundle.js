8e8c457904ffdcc619ac2a17816f41687bb77fdf
Michael Clayton Noble <makos.code@gmail.com>
2021-02-06 00:00:32 -0800

    add link to pokemon index item list of pokemon

--- W11D3/pokedex_part_2_attempt_2/app/assets/javascripts/bundle.js
+++ W11D3/pokedex_part_2_attempt_2/app/assets/javascripts/bundle.js
@@ -265,15 +265,19 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "PokemonIndexItem", function() { return PokemonIndexItem; });
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router-dom/esm/react-router-dom.js");
+
 
 var PokemonIndexItem = function PokemonIndexItem(_ref) {
   var poke = _ref.poke;
-  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Link"], {
+    to: "/pokemon/".concat(poke.id)
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
     className: "pokemon-index-item",
     key: poke.id
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", null, poke.id), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
     src: poke.imageUrl
-  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", null, poke.name));
+  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", null, poke.name)));
 };
 
 /***/ }),
--- W11D3/pokedex_part_2_attempt_2/frontend/components/pokemon/pokemon_index_item.jsx
+++ W11D3/pokedex_part_2_attempt_2/frontend/components/pokemon/pokemon_index_item.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 
+import {Link} from 'react-router-dom';
+
 export const PokemonIndexItem = ( {poke}) => {
-
-
   return (
-    <li className="pokemon-index-item" key={poke.id}>
-      <span>{poke.id}</span>
-      <img src={poke.imageUrl} />
-      <span>{poke.name}</span>
-    </li>
+    <Link to={`/pokemon/${poke.id}`}>
+
+      <li className="pokemon-index-item" key={poke.id}>
+        <span>{poke.id}</span>
+        <img src={poke.imageUrl} />
+        <span>{poke.name}</span>
+      </li>
+    </Link>
   )
 
 }
