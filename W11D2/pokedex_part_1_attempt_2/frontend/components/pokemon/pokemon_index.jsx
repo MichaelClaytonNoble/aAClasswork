8e5271d11a79fc34acd919b970e5608484ef4e9e
Michael Clayton Noble <makos.code@gmail.com>
2021-02-05 23:16:56 -0800

    create rendering a list of all of our pokemon

--- W11D2/pokedex_part_1_attempt_2/frontend/components/pokemon/pokemon_index.jsx
+++ W11D2/pokedex_part_1_attempt_2/frontend/components/pokemon/pokemon_index.jsx
@@ -2,11 +2,31 @@ import React from 'react';
 
 class PokemonIndex extends React.Component{
   constructor(props){
-
+    super(props);
   };
+  componentDidMount(){
+    this.props.requestAllPokemon();
+  }
   render(){
+    let {pokemon} = this.props;
+    pokemon = pokemon || [];
     return (
-      <div>hello</div>
+      <ul className="pokemon-index">
+      {
+        
+        pokemon.map( poke => {
+          return (
+            <li key={poke.id} className="pokemon-index-item">
+              {poke.name}
+              <img src={poke.imageUrl}></img>
+            </li>
+          )
+        })
+        
+      }
+      </ul>
     );
   };
 }
+
+export default PokemonIndex;
--- W11D2/pokedex_part_1_attempt_2/frontend/components/root.jsx
+++ W11D2/pokedex_part_1_attempt_2/frontend/components/root.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import {Provider} from 'react-redux';
+import PokemonIndexContainer from './pokemon/pokemon_index_container';
 
 const Root = ({store})=>(
+  <div className="pokedex">
+
   <Provider store={store}>
-    <div></div>
+    <PokemonIndexContainer />
   </Provider>
+  </div>
 );
 
 export default Root;
